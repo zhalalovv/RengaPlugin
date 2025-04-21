092cff4e172b81e562314361b722893a42730780
Vitaliy <zhalalov9@mail.ru>
2025-04-21 19:20:37 +0300

    Изменен UI

--- DoorWindowForm.Designer.cs
+++ DoorWindowForm.Designer.cs
@@ -7,13 +7,6 @@ namespace RengaPlugin
     {
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.ListBox listBoxParams;
-        private System.Windows.Forms.ComboBox comboBoxType;
-        private System.Windows.Forms.Label labelWidth;
-        private System.Windows.Forms.Label labelHeight;
-        private System.Windows.Forms.Label labelOffset;
-        private System.Windows.Forms.TextBox textBoxWidth;
-        private System.Windows.Forms.TextBox textBoxHeight;
-        private System.Windows.Forms.TextBox textBoxOffset;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnAdd;
 
@@ -27,13 +20,6 @@ namespace RengaPlugin
         private void InitializeComponent()
         {
             this.listBoxParams = new System.Windows.Forms.ListBox();
-            this.comboBoxType = new System.Windows.Forms.ComboBox();
-            this.labelWidth = new System.Windows.Forms.Label();
-            this.labelHeight = new System.Windows.Forms.Label();
-            this.labelOffset = new System.Windows.Forms.Label();
-            this.textBoxWidth = new System.Windows.Forms.TextBox();
-            this.textBoxHeight = new System.Windows.Forms.TextBox();
-            this.textBoxOffset = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -48,66 +34,10 @@ namespace RengaPlugin
             this.listBoxParams.Size = new System.Drawing.Size(360, 160);
             //this.listBoxParams.SelectedIndexChanged += new System.EventHandler(this.listBoxParams_SelectedIndexChanged);
             // 
-            // comboBoxType
-            // 
-            this.comboBoxType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboBoxType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxType.Items.AddRange(new object[] {
-            "Окно",
-            "Дверь"});
-            this.comboBoxType.Location = new System.Drawing.Point(12, 180);
-            this.comboBoxType.Size = new System.Drawing.Size(360, 21);
-            // 
-            // labelWidth
-            // 
-            this.labelWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelWidth.AutoSize = true;
-            this.labelWidth.Location = new System.Drawing.Point(12, 210);
-            this.labelWidth.Text = "Ширина";
-            // 
-            // textBoxWidth
-            // 
-            this.textBoxWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxWidth.Location = new System.Drawing.Point(12, 230);
-            this.textBoxWidth.Size = new System.Drawing.Size(360, 20);
-            // 
-            // labelHeight
-            // 
-            this.labelHeight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelHeight.AutoSize = true;
-            this.labelHeight.Location = new System.Drawing.Point(12, 260);
-            this.labelHeight.Text = "Высота";
-            // 
-            // textBoxHeight
-            // 
-            this.textBoxHeight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxHeight.Location = new System.Drawing.Point(12, 280);
-            this.textBoxHeight.Size = new System.Drawing.Size(360, 20);
-            // 
-            // labelOffset
-            // 
-            this.labelOffset.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelOffset.AutoSize = true;
-            this.labelOffset.Location = new System.Drawing.Point(12, 310);
-            this.labelOffset.Text = "Отступ";
-            // 
-            // textBoxOffset
-            // 
-            this.textBoxOffset.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxOffset.Location = new System.Drawing.Point(12, 330);
-            this.textBoxOffset.Size = new System.Drawing.Size(360, 20);
-            // 
             // buttonSave
             // 
             this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnSave.Location = new System.Drawing.Point(12, 370);
+            this.btnSave.Location = new System.Drawing.Point(12, 180);
             this.btnSave.Size = new System.Drawing.Size(90, 30);
             this.btnSave.Text = "Сохранить";
             this.btnSave.Click += new System.EventHandler(this.SaveButton_Click);
@@ -122,17 +52,10 @@ namespace RengaPlugin
             // 
             // DoorWindowForm
             // 
-            this.ClientSize = new System.Drawing.Size(384, 411);
-            this.MinimumSize = new System.Drawing.Size(400, 450);
-            this.MaximumSize = new System.Drawing.Size(700, 450);
+            this.ClientSize = new System.Drawing.Size(384, 220);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.MaximumSize = new System.Drawing.Size(700, 300);
             this.Controls.Add(this.listBoxParams);
-            this.Controls.Add(this.comboBoxType);
-            this.Controls.Add(this.labelWidth);
-            this.Controls.Add(this.textBoxWidth);
-            this.Controls.Add(this.labelHeight);
-            this.Controls.Add(this.textBoxHeight);
-            this.Controls.Add(this.labelOffset);
-            this.Controls.Add(this.textBoxOffset);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnAdd);
             this.Text = "Параметры дверей и окон";
